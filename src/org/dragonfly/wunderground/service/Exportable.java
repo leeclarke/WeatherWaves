bab21bc06b3efdd2cd2b4ce3f0b83145956f3c9c
Lee <lee.k.clarke@gmail.com>
2010-06-15 09:56:28 -0700

    added javadoc

--- src/org/dragonfly/wunderground/service/Exportable.java
+++ src/org/dragonfly/wunderground/service/Exportable.java
@@ -6,12 +6,21 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Indicates that the annotated field can be exported/imported to JSON/from xml
- *
+ * Indicates that the annotated field attribute can be exported/imported to JSON/from xml
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface Exportable {
+	/**
+	 * Xml tag name to use for parsing
+	 * @return
+	 */
 	String xmlName() default ""; // Won't accept null;
+	
+	
+	/**
+	 * Exported JSON name if different then xmlName
+	 * @return
+	 */
 	String jsonName() default "";
 }
