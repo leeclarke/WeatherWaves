5895e8eee0afc0fd579c129c51fb94f9c24a3139
leeclarke <lee@foo.com>
2010-06-18 23:08:10 -0400

    Robot Setup

---
+++ src/org/dragonfly/weatherwave/WeatherWaveRobot.java
@@ -0,0 +1,22 @@
+package org.dragonfly.weatherwave;
+
+import com.google.wave.api.AbstractRobot;
+
+
+public class WeatherWaveRobot extends AbstractRobot
+{
+
+	@Override
+	protected String getRobotName()
+	{
+		return "WeatherWaves Robot";
+	}
+
+	@Override
+	protected String getRobotProfilePageUrl()
+	{
+		return null;
+	}
+
+	
+}
