10a9ce2b33d239257871ec26ae9ada802cb961fb
Lee <lee.k.clarke@gmail.com>
2010-06-21 13:47:29 -0700

    Stubbed out change Event

--- src/org/dragonfly/weatherwave/WeatherWaveRobot.java
+++ src/org/dragonfly/weatherwave/WeatherWaveRobot.java
@@ -2,12 +2,14 @@ package org.dragonfly.weatherwave;
 
 import com.google.wave.api.AbstractRobot;
 import com.google.wave.api.Blip;
+import com.google.wave.api.event.DocumentChangedEvent;
 import com.google.wave.api.event.WaveletParticipantsChangedEvent;
 import com.google.wave.api.event.WaveletSelfAddedEvent;
 
 public class WeatherWaveRobot extends AbstractRobot
 {
-
+	//TODO: Ensure only one match at a time. Currently it could match multiples at once.
+	public static final String WW_REG_PATTERN = "@WW\\[.{4}.*\\]";//    regex:  @WW\[.....*\]
 	@Override
 	protected String getRobotName()
 	{
@@ -39,5 +41,16 @@ public class WeatherWaveRobot extends AbstractRobot
 		{
 			Blip blip = event.getWavelet().reply("\nHi : " + newParticipant);
 		}
+	}
+	
+	// Note that "\" is an escape character in Java strings, so it must be double escaped.  @WW[33584]      
+	//@WW[123]
+	//@WW[]
+	//TODO: Determine if need a terminator on the WW call.
+	@Capability(filter = WW_REG_PATTERN)
+	@Override
+	public void onDocumentChanged(DocumentChangedEvent event)
+	{
+		//TODO: Handle WW call.
 	}
 }
